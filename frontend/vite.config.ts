4f9a123b7b1f138d300d7b026606673393fcb3ae
Kdwan004 <kush.dewan004@gmail.com>
2025-05-08 01:30:12 +1000

    Setup for github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['20a2-103-214-20-216.ngrok-free.app']
-  }
+  base: '/frontend-web-com-app/', // Replace with your repository name
 });
