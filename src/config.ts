0cf17354ce34c5f46d8080373d024aac17e0ea30
Kdwan004 <kush.dewan004@gmail.com>
2025-05-08 14:11:02 +1000

    switched api endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://10.96.0.71:8080"; // https://web-app-image-320303374114.australia-southeast1.run.app
+const API_BASE_URL = "https://web-app-image-320303374114.australia-southeast1.run.app"; // "http://10.96.0.71:8080"; 
 
 export default API_BASE_URL;
