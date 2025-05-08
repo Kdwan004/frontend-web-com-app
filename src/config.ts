e8e20c17b081bc7f7ece0a8130604df34e41c148
Kdwan004 <kush.dewan004@gmail.com>
2025-05-08 23:54:47 +1000

    complete... just had to switch api url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://web-app-image-320303374114.australia-southeast1.run.app";
-const API_BASE_URL = "http://192.168.1.232:8080/"; 
+const API_BASE_URL = "https://web-app-image-320303374114.australia-southeast1.run.app";
+// const API_BASE_URL = "http://192.168.1.232:8080/"; 
 
 
 export default API_BASE_URL;
